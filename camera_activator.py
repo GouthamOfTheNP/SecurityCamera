b5dd6038a332a86d661eb78ddfbd336cb011fa4c
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-12-08 21:36:16 -0600

    Update camera_activator.py

--- camera_activator.py
+++ camera_activator.py
@@ -9,7 +9,7 @@ time = datetime.datetime.now().minute
 DB_URL = ("https://vigilancesolutions.pythonanywhere.com/478cb55db6df5b5b4f9d38e081161edf"
           "/3ec984b8ebcb95979aafc140ab3175f7/afb3132ee624f40beae950c57ae0f3a5/5f16518ece37398319606c81556fc42b")
 KEY = str(requests.get("https://vigilancesolutions.pythonanywhere.com/5f16518ece37398319606c81556fc42b/key_generator/"
-                          "exfkey/445jgjdakeyfnk").json())
+                       "exfkey/445jgjdakeyfnk").json())
 headers = {
 	'X-Metadata-Key': KEY
 }
