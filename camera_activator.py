897e0430b452fee183377ffaee2d99aa787bb14e
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-12-08 21:35:29 -0600

    Update camera_activator.py

--- camera_activator.py
+++ camera_activator.py
@@ -1,17 +1,27 @@
 import requests
-
-DB_URL = ("https://vigilancesolutions.pythonanywhere.com/478cb55db6df5b5b4f9d38e081161edf"
-          "/3ec984b8ebcb95979aafc140ab3175f7/afb3132ee624f40beae950c57ae0f3a5/5f16518ece37398319606c81556fc42b/"
-          f"{requests.get("https://vigilancesolutions.pythonanywhere.com5f16518ece37398319606c81556fc42b/key_generator/"
-                          "exfkey/445jgjdakeyfnk").json()}")
+import datetime
 
 with open("device_id.txt", "r") as f:
 	info = f.readlines()
 	device = info[0]
 
-for username, email, devices in requests.get(DB_URL).json():
-	if devices:
-		with open("device_id.txt", "a") as f:
-			f.write(f"{username}\n")
-		with open("emails.txt", "w") as f:
-			f.write(f"{email}\n")
+time = datetime.datetime.now().minute
+DB_URL = ("https://vigilancesolutions.pythonanywhere.com/478cb55db6df5b5b4f9d38e081161edf"
+          "/3ec984b8ebcb95979aafc140ab3175f7/afb3132ee624f40beae950c57ae0f3a5/5f16518ece37398319606c81556fc42b")
+KEY = str(requests.get("https://vigilancesolutions.pythonanywhere.com/5f16518ece37398319606c81556fc42b/key_generator/"
+                          "exfkey/445jgjdakeyfnk").json())
+
+headers = {
+	'X-Metadata-Key': KEY
+}
+
+
+response = requests.post(DB_URL, headers=headers)
+print(response.text)
+if response.status_code == 200:
+	for username, email, devices in response.json():
+		if device in str(devices):
+			with open("device_id.txt", "a") as f:
+				f.write(f"{username}\n")
+			with open("emails.txt", "w") as f:
+				f.write(f"{email}\n")
