8d37f49e471c044361a0131a9650d9cab327f09d
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-12-08 21:35:52 -0600

    Update camera_activator.py

--- camera_activator.py
+++ camera_activator.py
@@ -10,14 +10,11 @@ DB_URL = ("https://vigilancesolutions.pythonanywhere.com/478cb55db6df5b5b4f9d38e
           "/3ec984b8ebcb95979aafc140ab3175f7/afb3132ee624f40beae950c57ae0f3a5/5f16518ece37398319606c81556fc42b")
 KEY = str(requests.get("https://vigilancesolutions.pythonanywhere.com/5f16518ece37398319606c81556fc42b/key_generator/"
                           "exfkey/445jgjdakeyfnk").json())
-
 headers = {
 	'X-Metadata-Key': KEY
 }
-
-
 response = requests.post(DB_URL, headers=headers)
-print(response.text)
+
 if response.status_code == 200:
 	for username, email, devices in response.json():
 		if device in str(devices):
